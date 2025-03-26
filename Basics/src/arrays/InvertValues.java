339f66ce2bf24d0c7215b47019b461aa4a69b3f0
guille1928 <150677095+guille1928@users.noreply.github.com>
2025-03-26 17:44:13 +0100

    Update InvertValues.java

--- Basics/src/arrays/InvertValues.java
+++ Basics/src/arrays/InvertValues.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class InvertValues {
 
 	
@@ -8,25 +10,39 @@ public class InvertValues {
 	
 	
 	public static void main(String[] args) {
-		
-		//read the numbers of the array in order
-		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
-		
-		System.out.println("In order ");
-		System.out.println("----------------------------");
-		for(int i = 0; i<numbers.length;i++) {
-			
-			System.out.print(" "+ numbers[i]);
-		}
-		System.out.println(" ");
-		System.out.println(" ");
-		System.out.println("Change the order ");
-		System.out.println("----------------------------");
-		//lets try change the order of the values 
-		for(int i = numbers.length-1;i>=0;i--) {
-		//we declare an int with the array length minus 1	
-			System.out.print(" "+ numbers[i]);
-		}
-	}
 	
+	//read the numbers of the array in order
+	int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+	
+	System.out.println("In order ");
+	System.out.println("----------------------------");
+	for(int i = 0; i<numbers.length;i++) {
+		
+		System.out.print(" "+ numbers[i]);
+		}
+	System.out.println(" ");
+	System.out.println(" ");
+	System.out.println("Change the order ");
+	System.out.println("----------------------------");
+	//lets try change the order of the values 
+	for(int i = numbers.length-1;i>=0;i--) {
+	//we declare an int with the array length minus 1	
+		System.out.print(" "+ numbers[i]);
+		}
+		
+	System.out.println(" ");
+	System.out.println(" ");
+	System.out.println("Max and min ");	
+	System.out.println("----------------------------");
+	
+	int maxNumber= Arrays.stream(numbers).max().getAsInt();
+	int minNumber= Arrays.stream(numbers).min().getAsInt();
+	
+	System.out.println("The max number is : " + maxNumber);
+	System.out.println("The min number is : " + minNumber);
+		
+		
+		
+	}
+		
 }
