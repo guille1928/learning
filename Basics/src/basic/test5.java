ca163c1710bba893bf8eb9f7bbf2a5dd08d993de
Guille <150677095+guille1928@users.noreply.github.com>
2025-10-01 15:18:12 +0200

    Update test5.java

--- Basics/src/basic/test5.java
+++ Basics/src/basic/test5.java
@@ -1,32 +1,46 @@
 package basic;
 
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class test5 {
 
 	
-	/*public static int promedio (int [] ) {
-	promedio = 0;
-	return promedio;	
-	}
-	*/
+
 	public static void main(String[] args) {
 		//pedimos 5 numeros al user
 		Scanner scn = new Scanner(System.in);
 		int n= 0;
-		int [] arrayNum;
-		do {
-		n= n+1;
-		System.out.println("Introduce un número  ");
-		int num= scn.nextInt();
-		arrayNum = {num};
-		}
-		while (n<5);
-		
-		int media= arrayNum.
+		int [] arrayNum = new int [5];
+		int [] arrayNum2 = new int [5];
 		
 		
-		System.out.println("El promedio de los numeros del array es " );
+		 do {
+			
+			System.out.println("Introduce un número para el primer array  ");
+			arrayNum2[n]= scn.nextInt();
+			n++;
+		 	}
+		while (n< arrayNum2.length);
+		
+		 
+		 for (int i=0;i<arrayNum.length;i++) {
+			 System.out.println("Dame un numero");
+			 arrayNum[i]=scn.nextInt();
+ 
+		 }
+		 
+	
+		double media = Arrays.stream(arrayNum).average().getAsDouble();
+		double media2 = Arrays.stream(arrayNum2).average().getAsDouble();
+		System.out.println("El promedio de los numeros del array es " + media);
+		System.out.println("El promedio de los numeros del array es " + media2);
+	
+		
+		scn.close();
+	
+	
 	}
 	
 	
