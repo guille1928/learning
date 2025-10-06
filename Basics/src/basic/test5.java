755e61862244f49bc7bfdcb77ad7c2d04e700c33
Guille <150677095+guille1928@users.noreply.github.com>
2025-10-06 16:33:16 +0200

    Update test5.java

--- Basics/src/basic/test5.java
+++ Basics/src/basic/test5.java
@@ -1,6 +1,9 @@
 package basic;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> bcbfdf8ce646ff16974aea2b543bd00f2abe2e59
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,6 +14,7 @@ public class test5 {
 	public static void main(String[] args) {
 		//pedimos 5 numeros al user
 		Scanner scn = new Scanner(System.in);
+<<<<<<< HEAD
 		int n= 0;
 		int [] arrayNum = new int [5];
 		int [] arrayNum2 = new int [5];
@@ -41,6 +45,32 @@ public class test5 {
 		scn.close();
 	
 	
+=======
+		
+		int [] arrayNum= new int [5];
+		int n= arrayNum[0];
+		do {
+		System.out.println("Introduce un número  ");
+		int num= scn.nextInt();
+		arrayNum[n] = num;
+		n++;
+		}
+		while (n<arrayNum.length);
+		//mecanismo 1 utilizando Arrays.Stream
+		double media= Arrays.stream(arrayNum).average().getAsDouble();
+		System.out.println("El promedio de los numeros del array es " + media);
+		
+		System.out.println("");
+		//mecanismo realizando un for
+		double suma= 0;
+		for (int i=0; i<arrayNum.length;i++) {
+			int n1 = arrayNum[i];
+			suma+=n1;
+			
+		}
+		
+		System.out.println("La otra media sería : "+ (suma/arrayNum.length));
+>>>>>>> bcbfdf8ce646ff16974aea2b543bd00f2abe2e59
 	}
 	
 	
